1d9a0770474320efaf004dd89800890a7c983db1
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-02 19:49:26 -0300

    feat: cria o endpoint GET /talker

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs').promises;
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -9,7 +10,14 @@ const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send('foguetinho');
+  response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkersJson = await fs.readFile('./talker.json');
+  const talkers = JSON.parse(talkersJson);
+  
+  res.status(HTTP_OK_STATUS).json(talkers);
 });
 
 app.listen(PORT, () => {
