08cb3d6b5825ce1fe6179e77af0cc21fafcab263
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-03 16:42:13 -0300

    feat: modifica a requesição da rota '/talker'

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fs = require('fs').promises;
+const talkerRoute = require('./routes/talkerRoute.js');
 
 const app = express();
 app.use(express.json());
@@ -12,12 +12,7 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (_req, res) => {
-  const talkersJson = await fs.readFile('./talker.json');
-  const talkers = JSON.parse(talkersJson);
-  
-  res.status(HTTP_OK_STATUS).json(talkers);
-});
+app.use('/talker', talkerRoute);
 
 app.listen(PORT, () => {
   console.log('Online');
