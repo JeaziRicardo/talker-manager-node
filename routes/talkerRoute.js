aa17eac87e7c69eaf43a53a17b852da2d99b1a9a
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-03 17:29:37 -0300

    feat: cria o endpoint GET '/talker/:id'

--- routes/talkerRoute.js
+++ routes/talkerRoute.js
@@ -3,12 +3,28 @@ const fs = require('fs').promises;
 
 const talkerRoute = express.Router();
 const HTTP_OK_STATUS = 200;
+const NOT_FOUND_STATUS = 404;
 
 talkerRoute.get('/', async (_req, res) => {
   const talkersJson = await fs.readFile('./talker.json');
   const talkers = JSON.parse(talkersJson);
   
   res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+talkerRoute.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const talkersJson = await fs.readFile('./talker.json');
+  const talkers = JSON.parse(talkersJson);
+
+  const talker = talkers.find((t) => t.id === +id);
+
+  if (!talker) {
+    return res.status(NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' }); 
+  }
+
+  res.status(HTTP_OK_STATUS).json(talker);
 });
 
 module.exports = talkerRoute;
