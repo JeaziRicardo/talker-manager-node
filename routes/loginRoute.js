09a2e4a21f0d40ac844f1086451d05176ed4ed18
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-03 22:59:46 -0300

    feat: valida o email e o password na rota '/login'

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const crypto = require('crypto');
+const validateEmail = require('../middlewares/validateEmail');
+const validatePassword = require('../middlewares/validatePassword');
 
 const loginRoute = express.Router();
 const HTTP_OK_STATUS = 200;
 
-loginRoute.post('/', (_req, res) => {
+loginRoute.post('/', validateEmail, validatePassword, (_req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
 
   res.status(HTTP_OK_STATUS).json({ token });
