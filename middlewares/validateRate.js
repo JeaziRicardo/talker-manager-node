e21695652d9597abd6fc89074ea398680eef44d7
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-04 18:32:10 -0300

    feat: cria um middleware para validação do 'rate' gerado no 'talker'

---
+++ middlewares/validateRate.js
@@ -0,0 +1,19 @@
+function validateRate(req, res, next) {
+  const { talk: { rate } } = req.body;
+
+  const BAD_REQUEST_STATUS = 400;
+  const MIN_RATE = 1;
+  const MAX_RATE = 5;
+
+  if (!rate) {
+    return res.status(BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "rate" é obrigatório' });
+  }
+  if (+rate < MIN_RATE || +rate > MAX_RATE) {
+    return res.status(BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+}
+
+module.exports = validateRate;
