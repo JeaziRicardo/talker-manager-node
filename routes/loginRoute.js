6fb1773b16386f69f4239a29cc5ca4d863925683
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-03 21:38:31 -0300

    feat: implementa os dados da requisição POST para '/login'

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const crypto = require('crypto');
 
 const loginRoute = express.Router();
+const HTTP_OK_STATUS = 200;
+
+loginRoute.post('/', (_req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+
+  res.status(HTTP_OK_STATUS).json({ token });
+});
 
 module.exports = loginRoute;
